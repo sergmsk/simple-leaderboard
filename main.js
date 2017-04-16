5ca34652f13fdba8ca12f621162231b9055d223e
KS <me@gmail.com>
2017-04-16 22:24:40 +0300

    addAccounts

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ if(Meteor.isClient){
         'submit form': function(event){
             event.preventDefault();//предотвращение поведения по умолчанию, т.е.обновление страницы при отправке формы
             var playerNameVar = event.target.playerName.value;//имя нового игрока
-            var playerPointsVar = event.target.playerPoints.value;//очки нового игрока
+            var playerPointsVar = +event.target.playerPoints.value;//очки нового игрока
             //console.log("Добавление игрока");
             //console.log(playerNameVar);
             //console.log(event.type);
